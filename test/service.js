b38c09ed7ba3a4924b361a281b47c8f97d3acc63
elvis7t <elvis7t@gmail.com>
2022-05-04 10:29:15 -0300

    Reconfigurando Test Simples

--- test/service.js
+++
@@ -1,7 +0,0 @@
-import {setTimeout} from 'timers/promises' 
-export default class Service {
-    async save(params) {//Simular uma chamada assincrona
-      await setTimeout(2000)// dois segundos
-      return '${params.id} save with success!'
-    }
-}
